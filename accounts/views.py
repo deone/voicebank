a1e86e5fe57cd18424c62f766768e71da56b2b1e
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-07-15 10:56:21 +0100

    User creation form and view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,29 @@
-# Create your views here.
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth import authenticate, login
 
-def join(request):
-    pass
+from accounts.forms import UserJoinForm
+
+def join(request, template='registration/index.html', form=UserJoinForm):
+    if request.method == "POST":
+	form = form(request.POST)
+	if form.is_valid():
+	    form.save()
+
+	    # Authenticate and log user in.
+	    username = request.POST['username']
+	    password = request.POST['password1']
+
+	    user = authenticate(username=username, password=password)
+	    if user is not None:
+		if user.is_active:
+		    login(request, user)
+		else:
+		    pass
+	    
+	    return redirect('core.views.upload')
+    else:
+	form = form()
+
+    return render_to_response(template, {'form': form},
+	    context_instance=RequestContext(request))
