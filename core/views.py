5c8139046fa4f7565275fc0247161dd92b059b57
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-07-15 10:57:02 +0100

    Display success message after clip upload

--- core/views.py
+++ core/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from core.forms import VoiceClipForm
 
 @login_required
 def upload(request, template='core/index.html', form=VoiceClipForm):
-    """ """
-
     if request.method == "POST":
 	form = form(request.POST, request.FILES)
 	if form.is_valid():
 	    form.save()
-	    redirect('core.views.index')
+	    messages.success(request, "Audio clip uploaded")
+	    return redirect('core.views.upload')
     else:
 	form = form()
     
