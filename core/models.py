fba29ceb65632f9b0db4e519ee12c8cd9b701eca
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-07-18 10:33:42 +0100

    Voice clip upload now done by user. User -> VoiceClip ForeignKey relationship.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class VoiceClip(models.Model):
+    user = models.ForeignKey('auth.User')
     voice_clip = models.FileField(upload_to='clips')
--- core/views.py
+++ core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from core.forms import VoiceClipForm
 
 @login_required
 def upload(request, template='core/index.html', form=VoiceClipForm):
+
     if request.method == "POST":
 	form = form(request.POST, request.FILES)
 	if form.is_valid():
@@ -14,7 +16,7 @@ def upload(request, template='core/index.html', form=VoiceClipForm):
 	    messages.success(request, "Audio clip uploaded")
 	    return redirect('core.views.upload')
     else:
-	form = form()
+	form = form(auto_id='%s', initial={'user': request.user.id})
     
     return render_to_response(template, {'form': form},
 	    context_instance=RequestContext(request))
