e3e42164dc7b76966cf94eef6292d6cd5168a426
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-07-18 14:28:52 +0100

    Display user's voice clips.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_list_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import Http404
 
 from core.forms import VoiceClipForm
+from core.models import VoiceClip
 
 @login_required
 def upload(request, template='core/index.html', form=VoiceClipForm):
@@ -17,6 +19,13 @@ def upload(request, template='core/index.html', form=VoiceClipForm):
 	    return redirect('core.views.upload')
     else:
 	form = form(auto_id='%s', initial={'user': request.user.id})
+
+    try:
+	voice_clips = get_list_or_404(VoiceClip, user=request.user)
+    except Http404:
+	voice_clips = None
     
-    return render_to_response(template, {'form': form},
-	    context_instance=RequestContext(request))
+    return render_to_response(template, {
+	    'form': form,
+	    'clips': voice_clips
+	}, context_instance=RequestContext(request))
