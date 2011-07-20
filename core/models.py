06c5c223a4b16f72da2f03a96fcacf82719f1c20
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-07-20 17:30:13 +0100

    Add name column to VoiceClip model. For simplicity, let's use different players instead of the playlist approach

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class VoiceClip(models.Model):
+    name = models.CharField(max_length=100)
     user = models.ForeignKey('auth.User')
     voice_clip = models.FileField(upload_to='clips')
