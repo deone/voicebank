d6d4dd178e2bea06c0b271eb1b5e8ad8e2e3cd81
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-08-02 09:39:35 +0100

    Added gender and birthday fields to Profile model. Remove create_profile() signal since we've opted to save Profile manually after user creation in account/forms.py. Add profile url routing.

--- accounts/models.py
+++ accounts/models.py
@@ -1,29 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-import random
-import string
-import datetime
+GENDER_CHOICES = (
+	("M", "Male"),
+	("F", "Female"),
+)
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
     slug = models.SlugField(unique=True, editable=False)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     about = models.CharField(max_length=255)
-    date_of_birth = models.DateField(default=datetime.datetime.now())
+    birthday = models.DateField()
     photo = models.ImageField(upload_to="profile_pics")
 
-    def save(self, *args, **kwargs):
-	email_id = self.user.email.split('@')[0]
-	rand_alpha = "".join(random.sample('%s%s' % (string.lowercase,
-	    string.digits), 4))
-
-	self.slug = '%s%s' % (email_id, rand_alpha)
-	super(Profile, self).save()
-
-def create_profile(sender, **kwargs):
-    created = kwargs['created']
-
-    if created:
-	Profile.objects.create(user=kwargs['instance'])
-
-models.signals.post_save.connect(create_profile, sender=User)
+    def __unicode__(self):
+	return self.user.username
