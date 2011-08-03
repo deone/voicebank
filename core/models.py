90f869731b94a84566df3ff33730a5eacb1287a5
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-08-03 14:05:16 +0100

    Add date_added and listens field to track recent clip additions and number of listens for each clip. Register VoiceClip in admin.

--- core/models.py
+++ core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+import datetime
 
 class VoiceClip(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey('auth.User')
-    is_active = models.BooleanField(default=False)
     voice_clip = models.FileField(upload_to='clips')
+    date_added = models.DateTimeField(default=datetime.datetime.now(), editable=False)
+    is_active = models.BooleanField(default=False)
+    listens = models.IntegerField()
