377985be171bef34eb4f14d1b648ddba3ab62046
Oladayo Osikoya <alwaysdeone@gmail.com>
2011-08-10 15:32:35 +0100

    Remove profile_edit url. Reduce VoiceClip.name.max_length to 30 from 100.

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ import datetime
 
 class VoiceClip(models.Model):
     user = models.ForeignKey('auth.User')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
     voice_clip = models.FileField(upload_to='clips')
     language = models.CharField(max_length=30)
     date_added = models.DateTimeField(default=datetime.datetime.now(), editable=False)
