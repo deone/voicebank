6b6b365a009c3805f07905894b6ac2976afda9aa
Oladayo Osikoya <alwaysdeone@gmail.com>
2012-02-07 14:45:59 +0100

    Stop using root as DB user.

--- settings.py
+++ settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'voicebank',                      # Or path to database file if using sqlite3.
-	'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'dune369',                  # Not used with sqlite3.
+	'USER': 'voice_admin',                      # Not used with sqlite3.
+        'PASSWORD': 'v01ce',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,4 @@
+
 grant all on voicebank.* to 'voice_admin' identified by 'v01ce';
 
 create database voicebank;
