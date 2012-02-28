fd7f8250df1f5c8e3c31e72e3c30d26bdc061f6f
Oladayo Osikoya <alwaysdeone@gmail.com>
2012-02-28 14:26:19 +0100

    Make hosting-related settings changes in settings_live.py.

--- settings_live.py
+++ settings_live.py
@@ -1,3 +1,35 @@
 from settings import *
 
 DEBUG = TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'deone_voicebank',
+	'USER': 'deone_voicebank',
+        'PASSWORD': 'v01ce',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    'sorl.thumbnail',
+    'accounts',
+    'core',
+)
+
+STATICFILES_DIR = (
+    "/home/deone/webapps/voicebank_static/admin/",
+)
+STATIC_URL = "/site_media/"
+STATIC_ROOT = "/home/deone/webapps/voicebank_static/"
+
+ADMIN_MEDIA_PREFIX = "/site_media/admin/"
