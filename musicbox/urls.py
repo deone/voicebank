71b463ad01b820aa78cf351c044613c74225d6f8
Oladayo Osikoya <alwaysdeone@gmail.com>
2012-05-25 12:16:09 +0100

    Add pagination to album_list. * Create musicbox album pagination setting. * Pass setting into musicbox_home URL. * Display pagination on template.

--- musicbox/urls.py
+++ musicbox/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic import ListView, DetailView
+from django.conf import settings
 
 from musicbox.models import Album
 
 urlpatterns = patterns('musicbox.views',
 	url(r'^$', ListView.as_view(
 	    model=Album,
+	    paginate_by=settings.MUSICBOX_ALBUMS_PAGINATE_BY,
 	    ), name='musicbox_home'),
 	url(r'^(?P<slug>[-.\w]+)$', DetailView.as_view(
 	    model=Album,
