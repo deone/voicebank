c8c261a7d1b3bd69280867c3c761a21332875de1
Oladayo Osikoya <alwaysdeone@gmail.com>
2012-07-04 16:36:13 +0100

    Take context {{ MEDIA_URL }} in show_user_voiceclips template tag. Display comment count on musicbox album detail template.

--- vbank/templatetags/vbank_tags.py
+++ vbank/templatetags/vbank_tags.py
@@ -9,7 +9,8 @@ register = template.Library()
 def display_voiceclip_list(context):
     return context
 
-@register.inclusion_tag('vbank/list_of_voiceclips.html')
-def show_user_voiceclips(user, template):
+@register.inclusion_tag('vbank/list_of_voiceclips.html', takes_context=True)
+def show_user_voiceclips(context, user, template):
     clips = user.voiceclip_set.filter(is_active=True)
-    return {'voiceclip_list': clips, 'template': template, 'user': user}
+    return {'voiceclip_list': clips, 'template': template, 'user': user,
+	    'MEDIA_URL': context['MEDIA_URL']}
