6168f78a02c5d6da442f741b4df3353376e5920c
Dayo Osikoya <alwaysdeone@gmail.com>
2016-09-04 18:36:29 +0100

    Process ClipSearchForm POST data and display accurate page headers.

--- vbank/views.py
+++ vbank/views.py
@@ -8,8 +8,65 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from vbank.forms import * 
 from vbank.models import VoiceClip, Category
 
+import datetime
+
+AGE_GROUPS = {
+    'K': 'Kids',
+    'A': 'Adults'
+    }
+
+GENDER_STRING = {
+    'M': 'Men',
+    'F': 'Women'
+    }
+
 def clip_search(request, template='vbank/voiceclip_list.html', form=ClipSearchForm):
     voice_clips_list = VoiceClip.objects.active()
+
+    if request.method == 'POST':
+        language_string = 'All'
+        category_string = 'all categories'
+        gender_string = 'both genders in'
+        age_group_string = 'all age groups'
+
+        if request.POST['language']:
+            language = request.POST['language']
+            voice_clips_list = voice_clips_list.filter(language=request.POST['language'])
+            language_string = language
+
+        if request.POST['category']:
+            category = request.POST['category']
+            voice_clips_list = voice_clips_list.filter(category__pk=category)
+            category_string = 'the ' + Category.objects.get(pk=category).name + ' Category'
+
+        if request.POST['gender']:
+            gender = request.POST['gender']
+            voice_clips_list = voice_clips_list.filter(user__profile__gender=gender)
+            gender_string = dict(GENDER_STRING)[gender] + ' in'
+
+        if request.POST['age_group']:
+            today = datetime.date.today()
+            age_group = request.POST['age_group']
+            age_group_string = 'the ' + AGE_GROUPS[age_group] + ' age group'
+
+            clips_list = []
+            if age_group == 'K':
+                for clip in voice_clips_list:
+                    if clip.user.profile.birthday.year > (today.year - 15):
+                        clips_list.append(clip)
+            elif age_group == 'A':
+                for clip in voice_clips_list:
+                    if clip.user.profile.birthday.year < (today.year - 15):
+                        clips_list.append(clip)
+
+            voice_clips_list = clips_list
+
+        page_header = '%s voice clips by %s %s in %s' % (language_string, gender_string, age_group_string, category_string)
+    else:
+        form = form()
+        page_header = 'All Voice Clips'
+        # page_header = 'All voice clips by both genders in all age groups in all categories'
+
     paginator = Paginator(voice_clips_list, settings.VOICECLIP_LIST_PAGINATE_BY)
 
     page = request.GET.get('page')
@@ -20,14 +77,10 @@ def clip_search(request, template='vbank/voiceclip_list.html', form=ClipSearchFo
     except EmptyPage:
         voice_clips = paginator.page(paginator.num_pages)
 
-    if request.method == 'POST':
-        form = form(request.POST)
-    else:
-        form = form()
-
     return render_to_response(template, {
 	'voiceclip_list': voice_clips,
         'form': form,
+        'page_header': page_header
         }, context_instance=RequestContext(request))
 
 @login_required
