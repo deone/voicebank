de54d0b5fab5992caf638aa57932ef066d342974
Dayo Osikoya <alwaysdeone@gmail.com>
2016-10-02 00:37:57 +0100

    Remove obsolete template tag.

--- vbank/templatetags/vbank_tags.py
+++ vbank/templatetags/vbank_tags.py
@@ -8,9 +8,3 @@ register = template.Library()
 @register.inclusion_tag('vbank/clip.html', takes_context=True)
 def display_clip(context, clip):
     return {'STATIC_URL': context['STATIC_URL'], 'MEDIA_URL': context['MEDIA_URL'], 'clip': clip}
-
-@register.inclusion_tag('vbank/list_of_voiceclips.html', takes_context=True)
-def show_user_voiceclips(context, user, template):
-    clips = user.voiceclip_set.filter(is_active=True)
-    return {'voiceclip_list': clips, 'template': template, 'user': user,
-	    'MEDIA_URL': context['MEDIA_URL']}
