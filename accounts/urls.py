07fbdfa7a3abe2b818c0e95a43d70ea55f3f161d
Dayo Osikoya <alwaysdeone@gmail.com>
2016-10-04 16:11:58 +0100

    - Password reset URL and template. - Style main-wrap.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,10 @@ from accounts.views import ProfileDetailView
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'^login/$', auth_views.login,
       {'authentication_form': LoginForm, 'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^password_reset/$', auth_views.password_reset, {
+      'template_name': 'accounts/password_reset.html',
+    }, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, {}, name='password_reset_done'),
 )
 
 urlpatterns += patterns('accounts.views', 
