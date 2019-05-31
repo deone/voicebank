19ef04867d89d24e9da840ff45f0fc2acbbe1210
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-05-31 16:00:27 +0000

    Change dev database name because we moved dev server from AWS Cloud9 to WebFaction.

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'voicebank',                      # Or path to database file if using sqlite3.
-	'USER': 'voice_admin',                      # Not used with sqlite3.
+        'NAME': 'voicebank_dev',                      # Or path to database file if using sqlite3.
+        'USER': 'deone_voicebank',                      # Not used with sqlite3.
         'PASSWORD': 'v01ce',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
